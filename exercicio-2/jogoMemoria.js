1e5929e45f5e211db0241b0c1c6eab9ddf1780f7
Lucas SOuza <alfsouza708@gmail.com>
2018-10-08 18:48:57 -0300

    Adiciona jogo funcional

--- exercicio-2/jogoMemoria.js
+++ exercicio-2/jogoMemoria.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.card');
 
 let temCardVirado = false;
+let travaCard = false;
 let card1, card2;
 
 function virarCard() {
+    if(travaCard) return;
+    if(this === card1) return;
+
     this.classList.add('vira');
 
     if(!temCardVirado){
@@ -13,21 +17,47 @@ function virarCard() {
     }
 
     card2 = this;
-    temCardVirado = false;
-
     checaMatch();
 }
 
 function checaMatch() {
+    if(card1.dataset.meme === card2.dataset.meme) {
+        desativaCards();
+        return;
+    }
 
+    desviraCards();
 }
 
 function desativaCards() {
+    card1.removeEventListener('click', virarCard);
+    card2.removeEventListener('click', virarCard);
 
+    resetaCard();
 }
 
 function desviraCards() {
-    
+
+    travaCard = true;
+
+    setTimeout(() => {
+        card1.classList.remove('vira');
+        card2.classList.remove('vira');
+
+        resetaCard();
+    }, 750);
 }
+
+function resetaCard() {
+    [temCardVirado, travaCard] = [false, false];
+    [card1, card2] = [null, null];
+}
+
+(function embaralha() {
+    cards.forEach(card => {
+      let posRandom = Math.floor(Math.random() * 12);
+      card.style.order = posRandom;
+    });
+  })();
 
 cards.forEach(card => card.addEventListener('click', virarCard));
