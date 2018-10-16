9057d89d56a7c47433f1a0cc52419d083947443e
Lucas Souza <alfsouza708@gmail.com>
2018-10-15 23:53:04 -0300

    devolve titulo, desc e link da pagina

--- exercicio-3/ajax.js
+++ exercicio-3/ajax.js
@@ -1,19 +1,30 @@
 function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
-    var text, parser, xmlDoc;
-    xmlhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            text = xmlhttp.responseText;
+    var text = document.getElementById("textInput").value;
+    //var parser, xmlDoc;
+
+    console.log(text);
+
+    xmlhttp.onreadystatechange = function() { 
+        if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            parser = new DOMParser();
+            xmlDoc = parser.parseFromString(xmlhttp.responseText, "text/xml");
+            if (xmlDoc.getElementsByTagName("Description")[0]) {
+                document.getElementById("search1").innerHTML = xmlDoc.getElementsByTagName("Text")[0].innerHTML;
+
+                document.getElementById("search2").innerHTML = xmlDoc.getElementsByTagName("Description")[0].innerHTML;
+
+                document.getElementById("search3").innerHTML = xmlDoc.getElementsByTagName("Url")[0].innerHTML;
+
+                document.getElementById("search3").setAttribute('href',xmlDoc.getElementsByTagName("Url")[0].innerHTML);
+            }
+            else {
+                document.getElementById("search1").innerHTML = "Não tem resultados para a pesquisa.";
+            }
         }
-    };
-    xmlhttp.open("GET", "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&limit=1&search=" + document.getElementById("textInput") + "&format=xml", false);
+    }
+
+    xmlhttp.open("GET", "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&limit=1&search=<" + text + ">&format=xml", true);
     xmlhttp.send();
-
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(text, "text/xml");
-
-    document.getElementById("search").innerHTML = xmlDoc.getElementsByTagName()[0].childNodes[0].nodeValue;
+    console.log(text);
 }
-
-
-
