08ae7b49c2acf105b29090c6b8a68e51448d073a
Lucas Souza <alfsouza708@gmail.com>
2018-11-22 17:43:57 -0300

    Retorna titulo

--- trabalho-1/trab1.js
+++ trabalho-1/trab1.js
@@ -1,19 +1,36 @@
-
 function onLoadDoc() {
     var xmlhttp = new XMLHttpRequest();
     var url = "https://leandrojsa.github.io/movies.html";
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myContent = this.responseText;
-            t = "";
-            console.log("ENTROU PORA");
-            /*t = $(myContent).find('#id');
-            alert(t[0].html());*/
+            var myContext = this.responseText;
 
-            document.getElementById("destino").innerHTML = myContent;
+            console.log("ENTROU");
+            
+            if (myContext) {
+                console.log("ENTROU2");
+                retornaTitulo(myContext);                
+            }
+            else {
+                document.getElementById("destino").innerHTML = "Não tem filme";
+            }
         }
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+}
+
+function retornaTitulo(xml) {
+    var i;
+    var titles;
+    var parser=new DOMParser();
+    var xmlDoc=parser.parseFromString(xml,"text/html");
+    var x = xmlDoc.getElementsByTagName("div");
+
+    for(i = 0; i <= 2; i++) {
+        console.log(x);
+        titles += "<br>" + x[i].childNodes[3].innerHTML;
+    }
+    document.getElementById("destino").innerHTML = titles;
 }
